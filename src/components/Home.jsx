0de370dc2cfe93cc3265dbe13aa9988705b4d5d7
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-14 20:09:58 -0500

    create loadingSpinner, cryptoStats

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,13 @@ import {
   fetchCryptoFailure,
 } from "../redux/cryptoSlice/cryptoSlice"
 import { useDispatch, useSelector } from "react-redux"
+import { RingSpinner } from "./Spinner/Spinner"
+import CryptoStats from "./Crypto/CryptoStats"
 
 const Home = () => {
   const dispatch = useDispatch()
-  const {currentCryptos, loading} = useSelector(state=> state.cryptos)
-  console.log(currentCryptos)
+  const {loading} = useSelector(state=> state.cryptos)
+
   useEffect(() => {
     try {
       dispatch(fetchCryptoStart())
@@ -28,48 +30,17 @@ const Home = () => {
     }
   }, [dispatch])
 
+  if(loading) {
+    return (
+      <div className="h-[79vh] w-full flex items-center justify-center">
+        <RingSpinner />
+      </div>
+    )
+  }
+
   return (
     <div className="p-2">
-      <h1 className="text-2xl font-bold">Global Crypto Stats</h1>
-      {/* INFO */}
-      <div className="mt-5 flex w-full flex-col gap-5">
-        <div className="flex w-full  items-center gap-5">
-          <div className="flex w-1/2 flex-col">
-            <div>
-              <p className="text-sm text-slate-400">Total Cryptocurrencies</p>
-              <h1 className="text-lg font-semibold">10</h1>
-            </div>
-          </div>
-          <div className="flex w-1/2 flex-col">
-            <div>
-              <p className="text-sm text-slate-400">Total Exchanges</p>
-              <h1 className="text-lg font-semibold">10</h1>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full  items-center gap-5 ">
-          <div className="flex w-1/2 flex-col">
-            <div>
-              <p className="text-sm text-slate-400">Total Market Cap</p>
-              <h1 className="text-lg font-semibold">10</h1>
-            </div>
-          </div>
-          <div className="flex w-1/2 flex-col">
-            <div>
-              <p className="text-sm text-slate-400">Total 24h Volume</p>
-              <h1 className="text-lg font-semibold">10</h1>
-            </div>
-          </div>
-        </div>
-        <div className=" flex  w-full items-center gap-5">
-          <div className="flex w-1/2 flex-col">
-            <div>
-              <p className="text-sm text-slate-400">Total Markets</p>
-              <h1 className="text-lg font-semibold">10</h1>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CryptoStats />
     </div>
   )
 }
---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+import { RingLoader } from "react-spinners"
+
+const RingSpinner = () => {
+  return <RingLoader size={150} color="#000435"/>
+}
+
+export {RingSpinner}
