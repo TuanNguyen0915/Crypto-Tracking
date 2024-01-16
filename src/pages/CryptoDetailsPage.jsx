d3a99b47f3c25accef884fb09256c841fb0933c7
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-15 21:05:22 -0500

    Call api to get coinDetails

--- src/pages/CryptoDetailsPage.jsx
+++ src/pages/CryptoDetailsPage.jsx
@@ -1,7 +1,39 @@
+import { useParams } from "react-router-dom"
+import { getCoin } from "../services/cryptoApi"
+import { useEffect, useState } from "react"
+import { RingSpinner } from "../components/Spinner/Spinner"
 const CryptoDetailsPage = () => {
-  return (
-    <div>CryptoDetailsPage</div>
-  )
+  const { coinId } = useParams()
+  const [coin, setCoin] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(true)
+    try {
+      const fetchData = async () => {
+        const data = await getCoin(coinId)
+        setCoin(data.data.coin)
+      }
+      fetchData()
+    } catch (error) {
+      throw new Error(error)
+    }
+    setLoading(false)
+  }, [coinId])
+  if (loading) {
+    return (
+      <div className="flex h-[600px] w-full items-center justify-center">
+        <RingSpinner message="Loading coin" />
+      </div>
+    )
+  }
+
+  if(coin) {
+    return (
+      <div className="w-full flex flex-col justify-center gap-4">
+        {coin.name}
+      </div>
+    )
+  }
 }
 
 export default CryptoDetailsPage
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -6,7 +6,7 @@ const getCryptosQuery = async () => {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': import.meta.env.VITE_X_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+      'X-RapidAPI-Host': import.meta.env.VITE_X_RAPID_API_HOST
     }
   })
   const json = await res.json()
@@ -16,10 +16,26 @@ const getCryptosQuery = async () => {
 const getNews = async () => {
   const url = import.meta.env.VITE_NEWS_URL
   const res = await fetch(url, {
-    method:"GET"
+    method: "GET"
   })
   const json = await res.json()
   return json.results
 }
 
-export { getCryptosQuery, getNews }
+const getCoin = async (coinId) => {
+  const url = import.meta.env.VITE_COIN_BASE_URL
+
+  const res = await fetch(`${url}/${coinId}?timePeriod=7d`, {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_X_RAPID_API_KEY,
+      'X-RapidAPI-Host': import.meta.env.VITE_X_RAPID_API_HOST
+    }
+  }
+  )
+  const json = await res.json()
+  return json
+}
+
+
+export { getCryptosQuery, getNews, getCoin }
