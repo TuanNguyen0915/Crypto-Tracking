2c4431ea7b3dc9e77ea36b8b359d6e783e0d6bf7
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-16 00:22:58 -0500

    create crypto link component

--- src/components/Crypto/CryptoDetails/CryptoDetails.jsx
+++ src/components/Crypto/CryptoDetails/CryptoDetails.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react"
 import CryptoDetailsStats from "./CryptoDetailsStats"
-
+import CryptoLinks from "./CryptoLinks"
 
 const CryptoDetails = ({ coin }) => {
-  return (
-    <div className="mt-10 flex w-full flex-col justify-center gap-4">
-      <div className=" flex w-full flex-col items-center justify-center">
-        <h1 className="textTitle text-2xl">{coin.name} Value Statistic</h1>
-        <p className="textPara text-base">
-          An overview showing the stats of {coin.name}
-        </p>
+  const [isShow, setIsShow] = useState(false)
+  const coinColor = coin?.color ? `border border-[${coin.color}]` : "border"
+  if (coin) {
+    return (
+      <div className="mt-10 flex w-full flex-col justify-center gap-4 ">
+        <div className=" flex w-full flex-col items-center justify-center">
+          <button
+            className={`${coinColor} textTitle btn w-4/5 bg-blue-600 text-2xl text-white shadow-lg md:w-2/5`}
+            onClick={() => setIsShow(!isShow)}
+          >
+            {coin.name} Value Statistic
+          </button>
+          {isShow && (
+            <>
+              <p className="textPara mt-4 text-base">
+                An overview showing the stats of {coin.name}
+              </p>
+              {/* OVERVIEW */}
+              <CryptoDetailsStats coin={coin && coin} />
+            </>
+          )}
+        </div>
+        {/* COIN DETAILS */}
+        <div className="mt-10 flex w-full flex-col justify-center gap-4">
+          <h1 className="textTitle text-3xl text-blue-500">
+            What is {coin.name}
+          </h1>
+          <p>{coin.description}</p>
+          <h1 className="textTitle text-3xl text-blue-500">
+            {coin.name} Links
+          </h1>
+          <CryptoLinks coin={coin && coin} />
+        </div>
       </div>
-      {/* OVERVIEW */}
-      <CryptoDetailsStats coin={coin && coin} />
-    </div>
-  )
+    )
+  }
 }
 
 export default CryptoDetails
---
+++ src/components/Crypto/CryptoDetails/CryptoLinks.jsx
@@ -0,0 +1,23 @@
+const CryptoLinks = ({ coin }) => {
+  return (
+    <div className="flex w-full items-center">
+      <table className="table flex w-full items-center md:w-1/2">
+        <tbody className="w-full">
+          {coin.links?.map((link, idx) => (
+            <tr key={idx} className="flex w-full items-center justify-between hover:bg-slate-100 hover:rounded-md transition-all duration-300 ease-in my-1">
+              <td >{link.type}</td>
+              <td className="text-end">
+                <a href={link.url} target="_blank" rel="noreferrer"
+                className="textLink hover:text-blue-800">
+                  {link.name}
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default CryptoLinks
